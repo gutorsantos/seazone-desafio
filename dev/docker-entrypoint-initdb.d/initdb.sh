f4ed6eef8cde37df7be0126e10116106e6d83463
Gustavo Santos <gustavo.r.santos@outlook.com>
2022-11-16 00:58:58 -0300

    fixing dockerfile

--- dev/docker-entrypoint-initdb.d/initdb.sh
+++ dev/docker-entrypoint-initdb.d/initdb.sh
@@ -4,19 +4,25 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username ${POSTGRES_USER} --dbname ${POSTGRES_DB} <<-EOSQL
-    -- Cria o usuário
-    CREATE USER ${KHANTO_USER};
-    -- Define a senha
-    ALTER USER ${KHANTO_USER} WITH PASSWORD '${KHANTO_PASS}';
-    -- Define a codificação utilizada no tráfego com o cliente
-    ALTER ROLE ${KHANTO_USER} SET client_encoding TO 'utf8';
-    -- https://www.postgresql.org/docs/12/transaction-iso.html
-    ALTER ROLE ${KHANTO_USER} SET default_transaction_isolation TO 'read committed';
-    -- Utilizar horário UTC ao invés do horário brasileiro
-    ALTER ROLE ${KHANTO_USER} SET timezone TO 'UTC';
     -- Cria o Banco de Dados
     CREATE DATABASE ${KHANTO_DBNAME};
-    -- Dá privilégios no banco para o usuário do sistema
-    GRANT ALL PRIVILEGES ON DATABASE ${KHANTO_DBNAME} TO ${KHANTO_USER};
+
+    -- Cria o usuário
+    CREATE USER ${KHANTO_USER};
+    
+    -- Define a senha
+    ALTER USER ${KHANTO_USER} WITH PASSWORD '${KHANTO_PASS}';
+
+    \connect khantodb;
+    CREATE SCHEMA ${KHANTO_SCHEMA} AUTHORIZATION ${KHANTO_PASS};
+
+    -- Define a codificação utilizada no tráfego com o cliente
+    ALTER ROLE ${KHANTO_USER} SET client_encoding TO 'utf8';
+    
+    -- https://www.postgresql.org/docs/12/transaction-iso.html
+    ALTER ROLE ${KHANTO_USER} SET default_transaction_isolation TO 'read committed';
+    
+    -- Utilizar horário UTC ao invés do horário brasileiro
+    ALTER ROLE ${KHANTO_USER} SET timezone TO 'UTC';
 EOSQL
 
